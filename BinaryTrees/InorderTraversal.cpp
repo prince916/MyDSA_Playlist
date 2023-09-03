2ce349e280776795e44252d37f211a754feb90b3
Your Name <you@example.com>
2023-09-03 18:58:32 +0530

    updatenewfiles

---
+++ BinaryTrees/InorderTraversal.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node {
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node * newNode(int data){
+    struct node *node = (struct node *)malloc(sizeof(struct node));
+    node -> data = data;
+    node -> left = NULL;
+    node -> right = NULL;
+
+    return (node);
+}
+
+void inOrderTrav(node *currElement, vector <int> &inOrder){
+    if(currElement == NULL){
+        return;
+    }
+
+    inOrderTrav(currElement -> left, inOrder);
+    inOrder.push_back(currElement -> data);
+    inOrderTrav(currElement -> right, inOrder);
+}
+
+
+
+int main()
+{
+    struct node *root = newNode(1);
+    root -> left = newNode(2);
+    root -> right = newNode(3);
+    root -> left -> left = newNode(4);
+    root -> left -> right = newNode(5);
+    root -> left -> right -> left = newNode(8);
+    root -> right -> left = newNode(6);
+    root -> right -> right = newNode(7);
+    root -> right -> right -> left = newNode(9);
+    root -> right -> right -> right = newNode(10);
+
+
+    vector <int> inOrder;
+    inOrderTrav(root,inOrder);
+
+    cout << "The Inorder Traversal is : ";
+    for (int i = 0; i < inOrder.size(); i++)
+    {
+        cout << inOrder[i] << " ";
+    }
+    return 0;
+}
---
+++ BinaryTrees/PostorderTraversal.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node * newNode(int data){
+    struct node *node = (struct node *)malloc(sizeof(struct node));
+    node -> data = data;
+    node -> left = NULL;
+    node -> right = NULL;
+
+    return (node);
+}
+
+void postOrderTraversal(node *currElement,vector <int> &postOrder){
+    if(currElement == NULL){
+        return;
+    }
+
+    postOrderTraversal(currElement -> left, postOrder);
+    postOrderTraversal(currElement -> right, postOrder);
+    postOrder.push_back(currElement -> data);
+}
+
+
+int main()
+{
+    struct node *root = newNode(1);
+    root -> left = newNode(2);
+    root -> right = newNode(3);
+    root -> left -> left = newNode(4);
+    root -> left -> right = newNode(5);
+    root -> left -> right -> left = newNode(8);
+    root -> right -> left = newNode(6);
+    root -> right -> right = newNode(7);
+    root -> right -> right -> left = newNode(9);
+    root -> right -> right -> right = newNode(10);
+
+    vector <int> postOrder;
+    postOrderTraversal(root, postOrder);
+
+    cout << "The Postorder Traversal is : ";
+    for (int i = 0; i < postOrder.size(); i++)
+    {
+        cout << postOrder[i] << " ";
+    }
+    return 0;
+}
