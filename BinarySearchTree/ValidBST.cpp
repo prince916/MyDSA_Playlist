32e9989e363e441fc66f9c5a2e057c0b09bd68ab
Your Name <you@example.com>
2024-02-25 23:09:45 +0530

    newcommit

--- BinarySearchTree/ValidBST.cpp
+++ BinarySearchTree/ValidBST.cpp
@@ -52,13 +52,13 @@ int main()
     }
 
     Node* root2  = new Node(9);
-    root1->left = new Node(3);
-    root1->right = new Node(7);
-    root1->left->left = new Node(1);
-    root1->left->right = new Node(5);
-    root1->right->left = new Node(6);
-    root1->right->left = new Node(8);
-    root1->right->left->right = new Node(11);
+    root2->left = new Node(3);
+    root2->right = new Node(7);
+    root2->left->left = new Node(1);
+    root2->left->right = new Node(5);
+    root2->right->left = new Node(6);
+    root2->right->left = new Node(8);
+    root2->right->left->right = new Node(11);
 
     if(isBST(root2,NULL,NULL)){
         cout<<"valid BST"<<endl;
