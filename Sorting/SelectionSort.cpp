bff3edc162ff43c99a32672cafe063dddfb66ea6
Your Name <kumarprince08081999@gmail.com>
2024-04-10 23:28:33 +0530

    new commit

--- Sorting/SelectionSort.cpp
+++ Sorting/SelectionSort.cpp
@@ -23,6 +23,7 @@ void selectionSort(int arr[], int n){
 int main()
 {
     int n;
+    cout<< "Enter the number of array elements : " << endl;
     cin>>n;
     int arr[n];
     for (int i = 0; i < n; i++)
