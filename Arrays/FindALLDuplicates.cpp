cb21b54b5e1c4163ecbad404af64b1c53ca82fc4
Your Name <kumarprince08081999@gmail.com>
2024-04-28 13:05:48 +0530

    newcommit

--- Arrays/FindALLDuplicates.cpp
+++ Arrays/FindALLDuplicates.cpp
@@ -3,29 +3,23 @@ using namespace std;
 
 // Add Comment here
 
-int findAllDuplicates(vector<int> &arr, int n){
-    stack<int> st;
+int findDuplicates(vector<int> &arr){
+
+    int n = arr.size();
     sort(arr.begin(), arr.end());
 
-    for(int i=0; i<n; i++){
+    for(int i=0; i<n-1; i++){
         if(arr[i] == arr[i+1]){
             return arr[i];
-            st.push(arr[i]);
         }
     }
-
-    return -1;
-
 }
 
 int main()
 {
-    int n = 4;
-<<<<<<< HEAD
-    vector<int> arr = {1,1,2,3,4,5};
-=======
-    vector<int> arr = {1,1,2,3,4,4,5};
->>>>>>> 9a568a5ef3642c483c67b99f8b5a47701dfb3ed0
-    cout<<"Here is your Output"<< findAllDuplicates(arr,n) <<endl;
+    vector<int> arr;
+    arr = {1,3,3,4,5};
+    // vector<int> arr = {1,2,3,4,4,5};
+    cout << "Here is your Output : " << findDuplicates(arr) << endl;
     return 0;
 }
